87b532b2389ad4ee263f58629f20e5b90414916a
Sarthak Gupta <sarthakgupta98@gmail.com>
2021-03-18 21:56:45 +0530

    Adding swipe to refresh question details

--- app/src/main/java/com/demo/assignment/QuestionDetailsActivity.java
+++ app/src/main/java/com/demo/assignment/QuestionDetailsActivity.java
@@ -1,15 +1,20 @@
 package com.demo.assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class QuestionDetailsActivity extends AppCompatActivity {
 
@@ -18,6 +23,9 @@ public class QuestionDetailsActivity extends AppCompatActivity {
 
     private WebView webView;
     private ProgressBar progressCircular;
+    private TextView tvError;
+    private Button btnRetry;
+    private SwipeRefreshLayout swipeToRefresh;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -27,6 +35,9 @@ public class QuestionDetailsActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.web_view);
         progressCircular = findViewById(R.id.progress_circular);
+        tvError = findViewById(R.id.tv_error);
+        btnRetry = findViewById(R.id.btn_retry);
+        swipeToRefresh = findViewById(R.id.swipe_to_refresh);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
@@ -36,17 +47,36 @@ public class QuestionDetailsActivity extends AppCompatActivity {
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
-                progressCircular.setVisibility(View.VISIBLE);
+                swipeToRefresh.setVisibility(View.GONE);
                 webView.setVisibility(View.GONE);
+                progressCircular.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
+                swipeToRefresh.setRefreshing(false);
+                swipeToRefresh.setVisibility(View.VISIBLE);
                 progressCircular.setVisibility(View.GONE);
                 webView.setVisibility(View.VISIBLE);
             }
 
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                swipeToRefresh.setVisibility(View.GONE);
+                webView.setVisibility(View.GONE);
+                tvError.setVisibility(View.VISIBLE);
+                btnRetry.setVisibility(View.VISIBLE);
+            }
+        });
+
+        swipeToRefresh.setOnRefreshListener(() -> webView.loadUrl(questionLink));
+        btnRetry.setOnClickListener(v -> {
+            progressCircular.setVisibility(View.VISIBLE);
+            tvError.setVisibility(View.GONE);
+            btnRetry.setVisibility(View.GONE);
+            webView.loadUrl(questionLink);
         });
 
     }
